7327620320c391b1e32d4baa53c197623c7fe750
Gökdeniz Kaymaz <92694805+gokdenis@users.noreply.github.com>
2024-11-16 13:49:02 +0100

    fixed ChildList file problem

--- Assets/Scripts/ChildList.cs
+++ Assets/Scripts/ChildList.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ChildList : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Material[] Materials;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
