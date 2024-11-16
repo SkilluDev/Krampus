356c6abf86c9679d65cc1a8ad0e5f606f67a0abb
Gökdeniz Kaymaz <92694805+gokdenis@users.noreply.github.com>
2024-11-16 13:07:59 +0100

    Created Child List Manager

---
+++ Assets/Scripts/ChildrenListManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildrenListManager : MonoBehaviour
+{
+    public GameObject[] ChildPrefabs;
+
+    public Transform[] SpawnPoints;
+
+    public Color[] ShirtColors;
+
+    public int NpcCount = 14;
+    
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
