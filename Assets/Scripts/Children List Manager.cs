d2c518e3a017eb014fb850802c8c6538c62b057f
Gökdeniz Kaymaz <92694805+gokdenis@users.noreply.github.com>
2024-11-16 13:38:06 +0100

    created ChildList

--- Assets/Scripts/Children List Manager.cs
+++ Assets/Scripts/Children List Manager.cs
@@ -1,16 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChildrenListManager : MonoBehaviour
 {
-    public GameObject[] ChildPrefabs;
 
-    public Transform[] SpawnPoints;
+    public Material[] Materials;
 
-    public Color[] ShirtColors;
-
-    public int NpcCount = 14;
     
     void Start()
     {
