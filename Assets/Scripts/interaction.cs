cb888110667f9474707ef9838727fcd4b30ec411
SkilluDev <147891614+SkilluDev@users.noreply.github.com>
2024-11-17 13:35:52 +0100

    debug colored

--- Assets/Scripts/interaction.cs
+++ Assets/Scripts/interaction.cs
@@ -20,14 +20,14 @@ public class interaction : MonoBehaviour
 
     void Update()
     {
-        Vector3 worldPosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        Vector3 worldPosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         Vector3 upray = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
         Vector3 dir = ( upray - transform.position ).normalized;
         
-        //Debug.DrawLine(transform.position, worldPosition);
-        //Debug.DrawLine(worldPosition, upray);
-        Debug.DrawLine(transform.position, upray);
-        Debug.DrawRay(transform.position, dir);
+        Debug.DrawLine(transform.position, worldPosition, Color.green);
+        Debug.DrawLine(worldPosition, upray, Color.red);
+        Debug.DrawLine(transform.position, upray, Color.blue);
+        Debug.DrawRay(transform.position, dir, Color.yellow);
 
         RaycastHit hit;
 
