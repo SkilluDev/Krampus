497169e0efef8b9b90b0b8124743e3b37157ebb6
Michał Połosak <mpolosak@op.pl>
2024-11-17 17:41:25 +0100

    Fix ParentController

--- Assets/Scripts/ParentController.cs
+++ Assets/Scripts/ParentController.cs
@@ -6,11 +6,8 @@ using UnityEngine.AI;
 public class ParentController : MonoBehaviour
 {
     public float distance = 30;
-<<<<<<< Updated upstream
     private NavMeshAgent navMeshAgent;
-=======
     [SerializeField] private WinCondition winCondition;
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +17,7 @@ public class ParentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+        if (winCondition.isGamePausedValue())  return;
         var children = GameObject.FindGameObjectsWithTag("Child");
         foreach(GameObject child in children){
             detection detection = child.GetComponent<detection>();
@@ -30,22 +27,6 @@ public class ParentController : MonoBehaviour
             if (sqrDistanceToChild <= distance && !Physics.Raycast(transform.position, vector.normalized, distance*30,1<<6)) { 
                 detection.isAlerted = false;
                 navMeshAgent.SetDestination(detection.krampusEncounerPosition);
-=======
-        if (!winCondition.isGamePausedValue())
-        {
-            var children = GameObject.FindGameObjectsWithTag("Child");
-            foreach (GameObject child in children)
-            {
-                detection detection = child.GetComponent<detection>();
-                if (!detection.isAlerted) continue;
-                var vector = child.transform.position - transform.position;
-                var sqrDistanceToChild = Vector3.SqrMagnitude(vector);
-                if (sqrDistanceToChild <= distance &&
-                    !Physics.Raycast(transform.position, vector.normalized, distance * 30, 1 << 6))
-                {
-                    detection.isAlerted = false;
-                }
->>>>>>> Stashed changes
             }
         }
     }
