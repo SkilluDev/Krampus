9518d9a15ab85059c5c80b4d78bc6f0325086ca1
SkilluDev <147891614+SkilluDev@users.noreply.github.com>
2024-11-17 15:37:27 +0100

    tongue works perfectly

--- Assets/Scripts/interaction.cs
+++ Assets/Scripts/interaction.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class interaction : MonoBehaviour
@@ -10,6 +11,13 @@ public class interaction : MonoBehaviour
     private LineRenderer lineRenderer;
     private GameObject child;
     private TrailRenderer trail;
+
+    Ray ray;
+    RaycastHit hitData;
+    Vector3 testWorldPosition;
+    Vector3 dir;
+    
+    RaycastHit hit;
     
     void Start()
     {
@@ -24,32 +32,29 @@ public class interaction : MonoBehaviour
 
     void Update()
     {
-        Vector3 worldPosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 4));
-        Vector3 upray = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
-        Vector3 dir = ( upray - transform.position ).normalized;
-        
-        Debug.DrawLine(transform.position, worldPosition, Color.green);
-        Debug.DrawLine(worldPosition, upray, Color.red);
-        Debug.DrawLine(transform.position, upray, Color.blue);
-        Debug.DrawRay(transform.position, dir, Color.yellow);
-
-        RaycastHit hit;
-
         if (Input.GetButtonDown("Fire1"))
         {
-            if (Physics.Raycast(transform.position, dir, out hit , tongueLength, tonguable) && !Physics.Raycast(transform.position, dir,(hit.rigidbody.position - transform.position).magnitude, 1<<6))
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(ray, out hitData, 1000))
             {
-                //Vector3.Lerp(hit.rigidbody.position, transform.position, 0.5f);
-                child = hit.transform.gameObject;
-                trail.enabled = true;
-                trail.gameObject.transform.position = child.transform.position;
-                float time = 1f;
-                DOTween.To(()=>hit.rigidbody.position,(x)=>hit.rigidbody.position=x, transform.position, time).SetEase(Ease.InOutExpo);
-                DOTween.To(()=>trail.transform.position,(x)=>trail.transform.position=x, transform.position, time).SetEase(Ease.InOutExpo);
+                dir = ( hitData.transform.position - transform.position ).normalized;
                 
-                StartCoroutine(UpdateLineRenderer());
-                StartCoroutine(StopUpdateLineRenderer(time));
+                if (Physics.Raycast(transform.position, dir, out hit , tongueLength, tonguable) && !Physics.Raycast(transform.position, dir,(hit.rigidbody.position - transform.position).magnitude, 1<<6))
+                {
+                    child = hit.transform.gameObject;
+                    trail.enabled = true;
+                    trail.gameObject.transform.position = child.transform.position;
+                    float time = 1f;
+                    DOTween.To(()=>hit.rigidbody.position,(x)=>hit.rigidbody.position=x, transform.position, time).SetEase(Ease.InOutExpo);
+                    DOTween.To(()=>trail.transform.position,(x)=>trail.transform.position=x, transform.position, time).SetEase(Ease.InOutExpo);
+                
+                    StartCoroutine(UpdateLineRenderer());
+                    StartCoroutine(StopUpdateLineRenderer(time));
+                }
             }
+            
+            
 
         }
 
