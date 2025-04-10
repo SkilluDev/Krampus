80ead7e5420abd19aaaefe72f33f0b8b17d1aa5d
bajtix <bajtix.b@gmail.com>
2025-04-10 16:49:39 +0200

    rect generation + gizmos
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Text;
+using KrampUtils;
 using Roomgen;
 using UnityEngine;
 
@@ -9,7 +10,7 @@ public class RoomGenerator : MonoBehaviour {
 
     [SerializeField] private int m_width, m_height;
     [SerializeField] private RoomSet m_roomSet;
-
+    [SerializeField] private int m_loopRectangles;
     [SerializeField] private RoomType m_testType;
 
     private void Start() {
@@ -17,21 +18,8 @@ public class RoomGenerator : MonoBehaviour {
     }
 
     public void Generate() {
-
-
-
-
-
-
-        // foreach (var rt in m_roomSet.GetTierSortedList()) {
-        //     var possiblePlacements = new List<Vector2Int>();
-        //     while ((possiblePlacements = FindPossiblePlacements(rt)).Count > 0) {
-        //         PlaceRoom(rt, possiblePlacements[0]);
-        //     }
-
-        // }
-
-        // DebugLogDoorset();
+        CreateGrid();
+        DebugLogDoorset();
     }
 
     private void CreateGrid() {
@@ -40,6 +28,11 @@ public class RoomGenerator : MonoBehaviour {
         for (int i = 0; i < m_width; i++) for (int j = 0; j < m_height; j++) m_doorGrid[i, j] = new DoorFlags();
 
         void CreateRectangle(int sx, int sy, int ex, int ey) {
+            if (sx > ex) (ex, sx) = (sx, ex);
+            if (sy > ey) (ey, sy) = (sy, ey);
+            if (sx == ex) return;
+            if (sy == ey) return; // do not create zero width rects
+
             for (int i = sx, j = sy; i <= ex; i++) {
                 if (i != ex)
                     m_doorGrid[i, j].East = true;
@@ -68,8 +61,17 @@ public class RoomGenerator : MonoBehaviour {
                     m_doorGrid[i, j].South = true;
             }
         }
-        CreateRectangle(1, 1, 3, 3);
-        CreateRectangle(0, 0, 1, 1);
+
+        // create the spawn
+        m_generationGrid[m_width / 2, m_height / 2] = true;
+
+        for (int i = 0; i < m_loopRectangles; i++) {
+            int sx = Random.Range(0, m_width - 1);
+            int ex = Random.Range(sx + 1, m_width);
+            int sy = Random.Range(0, m_height - 1);
+            int ey = Random.Range(sy + 1, m_height);
+            CreateRectangle(sx, sy, ex, ey);
+        }
     }
 
     private List<Vector2Int> FindPossiblePlacements(RoomType room) {
@@ -124,4 +126,31 @@ public class RoomGenerator : MonoBehaviour {
         }
         Debug.Log(sb.ToString());
     }
+
+
+    #region Gizmos
+    private void OnDrawGizmosSelected() {
+        if (m_doorGrid == null) return;
+        Gizmos.color = Color.gray;
+        for (int i = 0; i < m_width; i++) {
+            for (int j = 0; j < m_height; j++) {
+                if (m_doorGrid[i, j] == null) continue;
+                var rd = m_doorGrid[i, j];
+
+                if (rd.North)
+                    Gizmos.DrawLine(Room.GetCellCenter(i, j), Room.GetCellCenter(i, j - 1));
+
+                if (rd.South)
+                    Gizmos.DrawLine(Room.GetCellCenter(i, j), Room.GetCellCenter(i, j + 1));
+
+                if (rd.East)
+                    Gizmos.DrawLine(Room.GetCellCenter(i, j), Room.GetCellCenter(i + 1, j));
+
+                if (rd.West)
+                    Gizmos.DrawLine(Room.GetCellCenter(i, j), Room.GetCellCenter(i - 1, j));
+            }
+        }
+    }
+
+    #endregion
 }
