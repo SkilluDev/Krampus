9a2006ceac17fa9926d89702cb166e539dd25072
WalkanFL <maciej.b.szczepanski@o2.pl>
2025-04-14 00:06:53 +0200

    Kamera, Krampus, Kid
    Camera:
    -Wider view when running (smooth change and reverts to original when sneaking / standing still)
    Krampus:
    -Shift is now for "sneaking"
    Child:
    -Revert Akkokiko change so that it works again

--- Assets/Scripts/Child.cs
+++ Assets/Scripts/Child.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Child : MonoBehaviour {
-    public bool isBad;
-    public Material mat;
+	public bool isBad;
+	public Material mat;
 
-    public MeshRenderer ring;
-    [SerializeField] private ParticleSystem confirmEatingParticle;
-    [SerializeField] private ParticleSystem confirmEatingParticleMistake;
-
-
-    private void Start() {
-        transform.GetChild(0).GetChild(8).GetComponent<Renderer>().material = mat;
-        transform.Rotate(transform.rotation.x, Random.Range(0, 360), transform.rotation.z);
-    }
-
-    public void Die() { }
+	public MeshRenderer ring;
+	[SerializeField] private ParticleSystem confirmEatingParticle;
+	[SerializeField] private ParticleSystem confirmEatingParticleMistake;
 
 
-    private void OnDestroy() {
-      //  if(gameObject.Scene.isLoaded){
-			//Instantiate(confirmEatingParticle, transform.position, Quaternion.identity);
-		//}			
-    }
+	private void Start() {
+		transform.GetChild(0).GetChild(8).GetComponent<Renderer>().material = mat;
+		transform.Rotate(transform.rotation.x, Random.Range(0, 360), transform.rotation.z);
+	}
+
+	public void Die() { }
+
+
+	private void OnDestroy() {
+		if (gameObject.scene.isLoaded) {
+			Instantiate(confirmEatingParticle, transform.position, Quaternion.identity);
+		}
+	}
 }
