5e749270aac17de6e2ea9908ca94651cf6881b8b
bajtix <bajtix.b@gmail.com>
2025-04-21 20:36:06 +0200

    make Krampus Behaviour abstract
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/KrampusBehaviour.cs
+++ Assets/Scripts/KrampusBehaviour.cs
@@ -1,11 +1,12 @@
 #if UNITY_EDITOR
+using KrampUtils;
 using NaughtyAttributes.Editor;
 using UnityEditor;
 #endif
 
 using UnityEngine;
 
-public class KrampusBehaviour : MonoBehaviour {
+public abstract class KrampusBehaviour : MonoBehaviour {
     public Krampus Kramp { get; private set; }
 }
 
@@ -17,7 +18,9 @@ public class KrampusBehaviourEditor : NaughtyInspector {
     private KrampusBehaviour Target => (KrampusBehaviour)target;
     public override void OnInspectorGUI() {
         if (Target.Kramp == null) {
-            EditorGUILayout.HelpBox("Krampus not found!", MessageType.Error);
+            if (EditorGUIHelper.HelpBoxWithButton("Krampus not found!", "Try again", MessageType.Error)) {
+                typeof(KrampusBehaviour).GetProperty(nameof(Target.Kramp)).SetValue(Target, Target.gameObject.GetComponentInParent<Krampus>());
+            }
         } else {
             EditorGUI.BeginDisabledGroup(true);
             EditorGUILayout.ObjectField("Krampus", Target.Kramp, typeof(Krampus), false);
