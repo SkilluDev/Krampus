d817c5280d785532af6698e9f3c07275212dbd07
bajtix <bajtix.b@gmail.com>
2025-04-22 13:20:21 +0200

    timed sequences
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/KrampUtils/TimedSequence.cs
+++ Assets/Scripts/KrampUtils/TimedSequence.cs
@@ -1,34 +1,58 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Linq;
+using UnityEngine;
 
 namespace KrampUtils {
 
     public class TimedSequence<T> {
-        private Dictionary<int, float> m_absolutes;
+        private readonly Dictionary<string, float> m_absolutes;
 
+        public TimedSequence() {
+            m_absolutes = new Dictionary<string, float>();
 
-        public TimedSequence(object t) {
-            // reflection magic
+            var sequentials = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance)
+                .Where(w => w.FieldType == typeof(float) && w.GetCustomAttribute<SeqDurationAttribute>() != null)
+                .OrderBy(w => w.GetCustomAttribute<SeqDurationAttribute>().Order);
+
+            var absolutes = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance)
+                .Where(w => w.FieldType == typeof(float) && w.GetCustomAttribute<SeqAbsoluteAttribute>() != null);
+
+            foreach (var w in absolutes) {
+                m_absolutes.Add(w.Name, (float)w.GetValue(this));
+            }
+
+            float accum = 0;
+            foreach (var w in sequentials) {
+                accum += (float)w.GetValue(this);
+                m_absolutes.Add(w.Name, accum);
+            }
+        }
+
+        public float this[string nameof] {
+            get => GetAbsolute(nameof);
         }
 
         public float GetAbsolute(string nameof) {
-            return 0;
+            if (!m_absolutes.ContainsKey(nameof)) throw new Exception("Invalid timing");
+            return m_absolutes[nameof];
         }
     }
 
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
-    public sealed class SeqDuration : Attribute {
+    public sealed class SeqDurationAttribute : Attribute {
         public int Order { get; }
-        public SeqDuration([CallerLineNumber] int order = 0) {
+        public SeqDurationAttribute([CallerLineNumber] int order = 0) {
             Order = order;
         }
 
     }
 
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
-    public sealed class SeqAbsolute : Attribute {
-        public SeqAbsolute() {
+    public sealed class SeqAbsoluteAttribute : Attribute {
+        public SeqAbsoluteAttribute() {
         }
 
     }
