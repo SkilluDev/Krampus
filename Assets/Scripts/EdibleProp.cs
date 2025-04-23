3192193bcb162e095321fb1ec25c7581f1fb558c
bajtix <bajtix.b@gmail.com>
2025-04-23 21:15:29 +0200

    Eating your mom
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/EdibleProp.cs
+++ Assets/Scripts/EdibleProp.cs
@@ -1,11 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class EdibleProp : MonoBehaviour, IEdible {
-    public void Consume(Krampus krampus) => throw new System.NotImplementedException();
-    public void Hit(Krampus krampus) => throw new System.NotImplementedException();
-    public void Prepare(Krampus krampus) => throw new System.NotImplementedException();
-    public void ReelIn(Krampus krampus, Vector3 position) => throw new System.NotImplementedException();
+    [SerializeField] private ParticleSystem m_consumeParticles;
+    [SerializeField] private Transform m_model;
 
+
+    private bool ModelSet => m_model != null;
+    private Vector3 m_initialScale;
+
+    [SerializeField]
+    [ShowIf("ModelSet")]
+    private AnimationCurve m_modelScale = new AnimationCurve(
+        new Keyframe(0f, 1f, 0f, 0f),
+        new Keyframe(1f, 0f, -90f, 0f, 0.01f, 0f)
+    );
+
+    private void Awake() {
+        if (ModelSet) {
+            m_initialScale = m_model.localScale;
+        }
+    }
+
+    public void Consume(Krampus krampus) {
+        if (m_consumeParticles != null) {
+            var particleObject = Instantiate(m_consumeParticles, transform.position, Quaternion.identity);
+            particleObject.gameObject.SetActive(true);
+        }
+        Destroy(gameObject);
+    }
+
+    public void Hit(Krampus krampus) {
+        foreach (var c in GetComponentsInChildren<Collider>()) c.enabled = false;
+        foreach (var c in GetComponentsInChildren<Rigidbody>()) c.isKinematic = true;
+    }
+    public void Prepare(Krampus krampus) { }
+    public void ReelIn(Krampus krampus, Vector3 position, float progress) {
+        transform.position = position;
+        if (ModelSet) {
+            m_model.localScale = m_initialScale * m_modelScale.Evaluate(progress);
+        }
+    }
 }
