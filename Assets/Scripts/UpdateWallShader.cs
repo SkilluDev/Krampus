ecf905be57ab1d157da4080cd781f06b8ac96398
WalkanFL <maciej.b.szczepanski@o2.pl>
2025-04-24 20:51:57 +0200

    WallShader Works Mostly as Intended
    and also walldoor opt door is also wall

--- Assets/Scripts/UpdateWallShader.cs
+++ Assets/Scripts/UpdateWallShader.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 [ExecuteAlways]
 public class UpdateWallShader : KrampusBehaviour {
-	[SerializeField] private Vector2 m_wallFade = new Vector2(-1, 1);
-	[SerializeField] private float m_smoothTime = 3f;
-	[SerializeField] private float m_offsetDistance = 10f;
-	[SerializeField] private float m_fadeOriginRayLength = 5f;
+	[SerializeField] private Vector2 m_wallFade = new Vector2(-5, 5);
+	[SerializeField] private float m_smoothTime = 5f;
+	[SerializeField] private float m_offsetDistance = 5f;
+	[SerializeField] private float m_fadeOriginRayLength = 10f;
+	private float m_distance = 0f;
 	private Vector3 m_fadeOriginPosition;
 	private void Update() {
-		float distance = -m_offsetDistance;
 		var aboveKrampus = Kramp.Kamera.Matrix.MultiplyVector(Vector3.forward);
 
 		if (Physics.Raycast(transform.position, aboveKrampus, out var hit, m_fadeOriginRayLength, 1 << 6)) {
-			distance = Mathf.Lerp(-m_offsetDistance, -m_offsetDistance - (m_fadeOriginRayLength - hit.distance), m_smoothTime * Time.deltaTime);
+			m_distance = Mathf.Lerp(m_distance, hit.distance - m_offsetDistance, m_smoothTime * Time.deltaTime);
+		} else {
+			m_distance = Mathf.Lerp(m_distance, m_offsetDistance, m_smoothTime * Time.deltaTime);
 		}
-		m_fadeOriginPosition = distance * aboveKrampus;
+		m_fadeOriginPosition = m_distance * aboveKrampus;
 
 
 		Shader.SetGlobalVector("_KrampusPosition", transform.position + m_fadeOriginPosition);
