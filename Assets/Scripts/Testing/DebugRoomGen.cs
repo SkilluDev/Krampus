7180d369d5469823936ec689b7d1a55eafb75b5c
bajtix <bajtix.b@gmail.com>
2025-04-26 22:38:50 +0200

    fix debugroomgen
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Testing/DebugRoomGen.cs
+++ Assets/Scripts/Testing/DebugRoomGen.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Roomgen;
 using UnityEngine;
 
 namespace KrampTests {
 
     public class DebugRoomGen : RoomGeneratorBase {
+        public override IReadOnlyCollection<Room> Rooms => throw new System.NotImplementedException();
 
+        public override void Generate() => throw new System.NotImplementedException();
+        public override Room GetRoomAt(Vector3 position) => throw new System.NotImplementedException();
     }
 
 }
