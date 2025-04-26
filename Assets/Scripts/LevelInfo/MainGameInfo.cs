42cce607129b8ec16364d6e2e54b39622aab761d
Akkokiko <Maciek.Polubiec@hotmail.com>
2025-04-26 20:57:08 +0200

    Adding Room Data

--- Assets/Scripts/LevelInfo/MainGameInfo.cs
+++ Assets/Scripts/LevelInfo/MainGameInfo.cs
@@ -1,22 +1,28 @@
 using System.Collections.Generic;
+using Roomgen;
 using UnityEngine;
 
 /// <summary>
 /// De-facto game controller for the main gaame scene
 /// </summary>
 public class MainGameInfo : LevelInfo {
-    public RoomGeneratorBase RoomGenerator => m_roomGenerator;
-    [SerializeField] private RoomGeneratorBase m_roomGenerator;
+	public RoomGeneratorBase RoomGenerator => m_roomGenerator;
+	[SerializeField] private RoomGeneratorBase m_roomGenerator;
 
-    public IReadOnlyCollection<Child> Children => m_childRegistry;
-    private List<Child> m_childRegistry;
+	public IReadOnlyCollection<Child> Children => m_childRegistry;
+	private List<Child> m_childRegistry;
 
+	private Dictionary<Room, RoomData> m_roomdata;
 
-    public void RegisterChild(Child child) {
-        m_childRegistry.Add(child);
-    }
+	public RoomData GetRoomData(Room r){
+		return m_roomdata[r];
+	}
 
-    public void UnregisterChild(Child child) {
-        m_childRegistry.Remove(child);
-    }
+	public void RegisterChild(Child child) {
+		m_childRegistry.Add(child);
+	}
+
+	public void UnregisterChild(Child child) {
+		m_childRegistry.Remove(child);
+	}
 }
