8249e601920a84ce58dbd353fce1871308b0590b
bajtix <bajtix.b@gmail.com>
2025-05-04 21:27:16 +0200

    move scripts about
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/InputSubscribe.cs
+++ Assets/Scripts/InputSubscribe.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using KrampInput;
+
 public class InputSubscribe {
 
 	static InputSubscribe() {
--- Assets/Scripts/KrampInput/FlickInteraction.cs
+++ Assets/Scripts/KrampInput/FlickInteraction.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-namespace InputExtension {
+namespace KrampInput {
 #if UNITY_EDITOR
     [InitializeOnLoad]
 #endif
