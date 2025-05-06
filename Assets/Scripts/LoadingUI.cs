fcfb8d1ba7e72f457edb331bebf5e50c85d48974
bajtix <bajtix.b@gmail.com>
2025-05-06 23:47:37 +0200

    Loading: General improvements
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/LoadingUI.cs
+++ Assets/Scripts/LoadingUI.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingUI : MonoBehaviour {
     [SerializeField] private GameLoader m_loader;
+    [SerializeField] private TextMeshProUGUI m_loadingText;
     [SerializeField] private TextMeshProUGUI m_statusText;
+    [SerializeField] private Slider m_slider;
 
     private void Update() {
-        m_statusText.text = m_loader.State;
+        m_statusText.text = m_loader.Status;
+        m_loadingText.text = Game.RequireFullReload ? "Loading" : "Debug Loading";
+        m_slider.value = m_loader.Progress;
     }
 
 }
--- Assets/Scripts/Testing/DebugLoadable.cs
+++ Assets/Scripts/Testing/DebugLoadable.cs
@@ -1,10 +1,27 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DebugLoadable : MonoBehaviour, IGameLoadable {
     public string Status { get; private set; }
 
     public float Progress { get; private set; }
+
+
+    private void Ready() {
+        Debug.Log("READY!");
+        InputSubscribe.Raw.Player.Crouch.performed += TempLoad;
+    }
+
+    private void Unready() {
+        Debug.Log("UNREADY!");
+        InputSubscribe.Raw.Player.Crouch.performed -= TempLoad;
+    }
+
+    private void TempLoad(InputAction.CallbackContext context) {
+        Game.LoadState(Game.State.MainMenu);
+    }
 
     public IEnumerator Load() {
         while (Progress < 0.5f) {
