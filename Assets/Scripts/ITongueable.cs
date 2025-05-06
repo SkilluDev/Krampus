4e3c87d5e5cd38ef17166fa0231bfca92e839b9c
bajtix <bajtix.b@gmail.com>
2025-05-06 19:34:36 +0200

    orange support
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/ITongueable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface ITongueable {
-    public GameObject GameObject => ((MonoBehaviour)this).gameObject;
-
-    public void TonguePassBy(Krampus krampus, Vector3 point, float tongueLength);
-    public void TongueHit(Krampus krampus, Vector3 point);
-}
--- Assets/Scripts/LevelInfo/LevelInfo.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class LevelInfo : MonoBehaviour
-{
-    public Game.State State => m_state;
-    [SerializeField] private Game.State m_state;
-
-}
