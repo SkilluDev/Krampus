892e5557e591ddb02f48e8d598354b057d5aff11
s32087 <s32087@pjwstk.edu.pl>
2025-05-07 13:57:23 +0200

    Small step tweak

--- Assets/Scripts/MusicMan.cs
+++ Assets/Scripts/MusicMan.cs
@@ -1,5 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicMan : MonoBehaviour {
+	[SerializeField] private AudioSource gameMusicLayer1;
+	[SerializeField] private AudioSource gameMusicLayer2;
+
+	public void Ready() {
+		Debug.Log("received broadcast");
+		if (Game.CurrentState==Game.State.MainGame) {
+
+			gameMusicLayer1.Play();
+		}
+	}
 
 }
