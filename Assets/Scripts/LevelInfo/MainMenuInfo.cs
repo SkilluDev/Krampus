874ddd7cb0c7483cfbf5e1675f79bd37ed244a36
bajtix <bajtix.b@gmail.com>
2025-05-07 19:10:52 +0200

    Main Menu: Settings
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/LevelInfo/MainMenuInfo.cs
+++ Assets/Scripts/LevelInfo/MainMenuInfo.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using LitMotion;
 using LitMotion.Extensions;
 using UnityEngine.Events;
+using System.Collections;
 
 public class MainMenuInfo : LevelInfo {
     public new enum State {
@@ -23,13 +24,16 @@ public class MainMenuInfo : LevelInfo {
 
     private void Ready() {
         m_motions = new MotionHandle[m_canvases.Length];
+        foreach (var c in m_canvases) c.alpha = 0;
         SetState(State.Default);
         Debug.Log("Set state default!");
     }
 
     private void Unready() {
-        foreach (var motion in m_motions)
-            if (motion.IsActive()) motion.Cancel();
+        Debug.Log("Kill motions");
+        for (int i = 0; i < m_motions.Length; i++) {
+            m_motions[i].TryCancel();
+        }
     }
 
     public void SetState(State state) {
@@ -40,7 +44,13 @@ public class MainMenuInfo : LevelInfo {
     }
 
     public void LoadGameScene() {
-        Game.LoadState(Game.State.MainGame);
+        IEnumerator Internal() {
+            SetState(State.Transitioning);
+            yield return new WaitForSecondsRealtime(1.2f);
+            Game.LoadState(Game.State.MainGame);
+        }
+
+        StartCoroutine(Internal());
     }
 
     // bad code ahead!
@@ -53,9 +63,8 @@ public class MainMenuInfo : LevelInfo {
             if (m_motions[i].IsActive()) m_motions[i].Cancel();
             if (i == (int)CurrentState) m_canvases[i].gameObject.SetActive(true);
             m_motions[i] = LMotion.Create(m_canvases[i].alpha, ij == (int)CurrentState ? 1f : 0f, 1f)
-                .WithOnComplete(
-                    () => m_canvases[ij].gameObject.SetActive(ij == (int)CurrentState)
-                ).BindToAlpha(m_canvases[ij]);
+                            .WithOnComplete(() => m_canvases[ij].gameObject.SetActive(ij == (int)CurrentState))
+                            .BindToAlpha(m_canvases[ij]);
         }
     }
     public void SetState(int state) => SetState((State)state);
