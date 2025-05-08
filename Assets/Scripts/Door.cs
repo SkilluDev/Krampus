2818307da4e2ff8418be1307c766546fdac9c72b
bajtix <bajtix.b@gmail.com>
2025-05-08 02:14:24 +0200

    door
    Signed-off-by: bajtix <bajtix.b@gmail.com>

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable {
+    public void Interact(IInteractor interactor) {
+
+    }
+
+}
