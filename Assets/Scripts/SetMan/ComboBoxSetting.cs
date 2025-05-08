1c426739407c8e9d2da87ffced3eec1b3c9c8efb
bajtix <bajtix.b@gmail.com>
2025-05-08 14:58:56 +0200

    Settings: remove annoying debugs
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/SetMan/ComboBoxSetting.cs
+++ Assets/Scripts/SetMan/ComboBoxSetting.cs
@@ -11,9 +11,7 @@ namespace Settings {
             base.Cook();
             foreach (string w in options) m_dropdown.options.Add(new TMP_Dropdown.OptionData(w));
             m_dropdown.onValueChanged.AddListener(InputValueChanged);
-
-            m_dropdown.value = (int)(Int64)manager.GetValue(name);
-            Debug.Log("Set dropdown " + name + " = " + m_dropdown.value);
+            m_dropdown.value = (int)manager.GetValueIntegral(name);
             m_dropdown.RefreshShownValue();
         }
 
