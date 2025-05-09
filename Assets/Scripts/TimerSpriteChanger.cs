181a991c4084cbe8fb5024c0189d1ba1d241734a
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-05-09 02:32:54 +0200

    Setup for color feedback on timer

--- Assets/Scripts/TimerSpriteChanger.cs
+++ Assets/Scripts/TimerSpriteChanger.cs
@@ -7,6 +7,10 @@ public class TimerSpriteChanger : MonoBehaviour {
     [SerializeField] private int m_decimalPlace;
     [SerializeField] private Sprite[] m_spriteSheet;
     [SerializeField] private Image m_image;
+
+    [SerializeField] private Color m_normalColor = new Color(0x1B,0xC0,0x00);
+    [SerializeField] private Color m_goodColor = new Color(0xFF,0xFF,0xFF);
+    [SerializeField] private Color m_badColor = new Color(0xC0,0x00,0x09);
     private Sprite currentSprite;
 
     void Update() {
