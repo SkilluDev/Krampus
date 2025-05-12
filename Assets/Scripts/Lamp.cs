da9e2390cbe28d17a53c68945088af5831aafa21
bajtix <bajtix.b@gmail.com>
2025-05-12 22:31:35 +0200

    Misc: Lamp can be toggled
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Lamp.cs
+++ Assets/Scripts/Lamp.cs
@@ -1,21 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class Lamp : MonoBehaviour, IInteractable {
     [SerializeField] private Transform m_switch;
     [SerializeField] private Light m_light;
     [SerializeField] private MeshRenderer m_renderer;
-
-
+    [SerializeField] private bool m_enabledByDefault = true;
     public Vector3 InteractionPoint => m_switch != null ? m_switch.position : transform.position;
 
-    public void Interact(IInteractor interactor) {
+    private void Start() {
+        if (!m_enabledByDefault) Toggle();
+    }
+
+    public void Toggle() {
         m_light.enabled = !m_light.enabled;
         if (m_light.enabled)
             m_renderer.material.EnableKeyword("_EMISSION");
         else
             m_renderer.material.DisableKeyword("_EMISSION");
+    }
+
+    public void Interact(IInteractor interactor) {
+        Toggle();
     }
 }
