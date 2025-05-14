31044bac02f7798b90916a2412861d1d8f79a9ef
bajtix <bajtix.b@gmail.com>
2025-05-15 00:45:55 +0200

    Misc: doors for idiots+better stunning
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,27 +1,24 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NaughtyAttributes;
 using Sound;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Door : Passage, IInteractable {
     public bool IsOpen { get; private set; }
 
     [SerializeField] private Animator m_animator;
     [SerializeField] private Collider m_blocking;
-    [SerializeField] private Collider m_interactionLeft;
-    [SerializeField] private Collider m_interactionRight;
     [SerializeField] private Sex m_doorClose;
     [SerializeField] private Sex m_doorOpen;
     [SerializeField] private float m_fastOpenObjectVelocity = 6f;
     [SerializeField] private float m_noiseDistance = 15f;
     [SerializeField][AnimatorParam(nameof(m_animator))] private int m_openProperty, m_openSuddenProperty, m_invertProperty;
-
+    [SerializeField] private Transform m_flap1, m_flap2;
     [SerializeField] private float m_stunDuration;
+    [SerializeField] private float m_stunLinger = 0.3f;
+
+    private float m_stunTime;
 
 
     private List<ICharacter> m_charactersInDoor = new List<ICharacter>();
@@ -32,35 +29,28 @@ public class Door : Passage, IInteractable {
 
     public bool CanInteract(IInteractor interactor) {
         if (interactor.InteractorType != IInteractor.Type.Player) return false;
-        m_hitRight =
-            Vector3.SqrMagnitude(interactor.AsPlayer().Tongue.HitPoint - m_interactionLeft.ClosestPoint(interactor.AsPlayer().Tongue.HitPoint)) >
-            Vector3.SqrMagnitude(interactor.AsPlayer().Tongue.HitPoint - m_interactionRight.ClosestPoint(interactor.AsPlayer().Tongue.HitPoint));
+        m_hitRight = Random.Range(0, 2) == 0;
         return IsOpen;
     }
 
-    public Vector3 InteractionPoint => m_hitRight ? m_interactionRight.transform.position + m_interactionRight.transform.up : m_interactionLeft.transform.position + m_interactionLeft.transform.up;
+    public Vector3 InteractionPoint => m_hitRight ? m_flap1.position : m_flap2.position;
 
-
-    [System.Obsolete]
-    private float CalculateVolumeOverride() {
-        return Mathf.InverseLerp(50, 20, Vector3.Distance(Game.MainGameInfo.Krampus.transform.position, transform.position)) * 0.5f;
-    }
 
     private void Start() {
         m_animator.SetBool(m_openProperty, false);
         m_blocking.enabled = true;
-        m_interactionLeft.enabled = false;
-        m_interactionRight.enabled = false;
+    }
+
+    private void Update() {
+        if (m_stunTime > 0) m_stunTime -= Time.deltaTime;
     }
 
     public void Interact(IInteractor interactor) {
         Close(true);
     }
 
-    // TODO: redo
     private void Open(bool swiftly, bool flip, ICharacter actor = null) {
         if (IsOpen) return;
-        // m_doorOpen.SetVolume(CalculateVolumeOverride());
         m_doorOpen.Play(transform.position, 1f);
         m_animator.SetBool(m_openSuddenProperty, swiftly);
         m_animator.SetBool(m_invertProperty, flip);
@@ -71,16 +61,15 @@ public class Door : Passage, IInteractable {
             Game.MainGameInfo.GetRoomData(A).MakeNoise(transform.position, m_noiseDistance, actor);
             Game.MainGameInfo.GetRoomData(B).MakeNoise(transform.position, m_noiseDistance, actor);
         }
-        //m_interactionLeft.enabled = true;
-        //m_interactionRight.enabled = true;
+
         IsOpen = true;
     }
 
     private void Close(bool swiftly, ICharacter actor = null) {
         if (!IsOpen) return;
-        // m_doorClose.SetVolume(CalculateVolumeOverride());
 
         if (swiftly) {
+            m_stunTime = m_stunLinger;
             foreach (var w in m_charactersInDoor) {
                 if (w is Nun n) n.Stun(m_stunDuration);
             }
@@ -90,8 +79,6 @@ public class Door : Passage, IInteractable {
         m_animator.SetBool(m_openSuddenProperty, swiftly);
         m_animator.SetBool(m_openProperty, false);
         m_blocking.enabled = true;
-        //m_interactionLeft.enabled = false;
-        //m_interactionRight.enabled = false;
         IsOpen = false;
     }
 
@@ -108,8 +95,12 @@ public class Door : Passage, IInteractable {
     }
 
     private void OnTriggerStay(Collider other) {
-        if (!other.TryGetComponent<Nun>(out var character)) return;
-        if (character.CurrentState != Nun.State.Stunned) Open(
+        if (!other.TryGetComponent<ICharacter>(out var character)) return;
+
+        if (character is Nun nun && nun.CurrentState == Nun.State.Stunned) {
+            return;
+        }
+        Open(
             character.VelocitySqr > m_fastOpenObjectVelocity * m_fastOpenObjectVelocity,
             Vector3.Dot(transform.forward, transform.position - other.transform.position) > 0
         );
@@ -118,6 +109,8 @@ public class Door : Passage, IInteractable {
     private void OnTriggerEnter(Collider other) {
         if (!other.TryGetComponent<ICharacter>(out var character)) return;
         m_charactersInDoor.Add(character);
+
+        if (character is Nun nun && m_stunTime > 0) nun.Stun(m_stunDuration);
 
         Open(
             character.VelocitySqr > m_fastOpenObjectVelocity * m_fastOpenObjectVelocity,
