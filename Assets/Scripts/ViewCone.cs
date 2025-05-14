feacb792d5c6469c9e82df22920c3167f6143f28
bajtix <bajtix.b@gmail.com>
2025-05-14 19:08:17 +0200

    AI: Viewcones use MeshRendering
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/ViewCone.cs
+++ Assets/Scripts/ViewCone.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using KrampUtils;
 using NaughtyAttributes;
 using Roomgen;
@@ -9,18 +10,20 @@ public class ViewCone : MonoBehaviour {
     [BoxGroup("Cone")] public Transform trackedObject;
     [BoxGroup("Cone")] public LayerMask visionMask;
 
-    [SerializeField] private LineRenderer m_renderer;
+    [SerializeField] private MeshFilter m_meshFilter;
+    [SerializeField] private MeshRenderer m_renderer;
     [SerializeField] private int m_additionalRaycastDelta = 1;
     [SerializeField] private float m_displaySmoothing = 4;
 
-    private Vector3[] m_targetConePositions = new Vector3[5];
-    private Vector3[] m_previousTargetConePositions = new Vector3[5];
-    private Vector3[] m_conePositions = new Vector3[5];
+    private Vector3[] m_targetConePositions = new Vector3[4];
+    private Vector3[] m_previousTargetConePositions = new Vector3[4];
+    private Vector3[] m_conePositions = new Vector3[4];
     private int m_frameCounter = 0;
 
-    public bool Detect() {
+    private bool m_enabled = true;
 
-        if (!m_renderer.enabled) return false;
+    public bool Detect() {
+        if (!m_enabled) return false;
         if (Vector3.SqrMagnitude(trackedObject.position - transform.position) > range * range) return false;
         if (Vector3.Angle(transform.forward, (trackedObject.transform.position - transform.position).NoY()) > fov / 2) return false;
 
@@ -37,6 +40,14 @@ public class ViewCone : MonoBehaviour {
 
     private void Awake() {
         m_frameCounter = Random.Range(0, m_additionalRaycastDelta);
+        m_meshFilter.mesh = new Mesh();
+
+        m_meshFilter.mesh.SetVertices(m_conePositions);
+        m_meshFilter.mesh.SetTriangles(new List<int>() {
+            0, 1, 2, 0, 2, 3
+        }, 0);
+        m_meshFilter.mesh.RecalculateNormals();
+        m_meshFilter.mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 15);
     }
 
     private Vector3 GetRaycastedPosition(float angle) {
@@ -50,12 +61,14 @@ public class ViewCone : MonoBehaviour {
         for (int i = 0; i < m_conePositions.Length; i++) {
             m_conePositions[i] = Vector3.Lerp(m_conePositions[i], Vector3.Lerp(m_previousTargetConePositions[i], m_targetConePositions[i], (m_frameCounter % m_additionalRaycastDelta) / (float)m_additionalRaycastDelta), Time.deltaTime * m_displaySmoothing);
         }
-        m_renderer.positionCount = m_conePositions.Length;
-        m_renderer.SetPositions(m_conePositions);
+
+        m_meshFilter.mesh.SetVertices(m_conePositions);
+        //m_renderer.positionCount = m_conePositions.Length;
+        //m_renderer.SetPositions(m_conePositions);
     }
 
     private void FixedUpdate() {
-        if (!m_renderer.enabled) return;
+        if (!m_meshFilter) return;
         m_frameCounter++;
         if (m_frameCounter % m_additionalRaycastDelta != 0) return;
         System.Array.Copy(m_targetConePositions, m_previousTargetConePositions, m_targetConePositions.Length);
@@ -63,10 +76,10 @@ public class ViewCone : MonoBehaviour {
         m_targetConePositions[1] = GetRaycastedPosition(-fov / 2f);
         m_targetConePositions[2] = GetRaycastedPosition(Mathf.Clamp(Vector3.SignedAngle(transform.forward, (trackedObject.transform.position - transform.position).NoY(), Vector3.up), -fov / 2f + 1f, fov / 2f - 1f));
         m_targetConePositions[3] = GetRaycastedPosition(fov / 2f);
-        m_targetConePositions[4] = new Vector3(0, Room.STANDARD_FLOOR_Y + 0.05f, 0);
     }
 
     public void SetActive(bool v) {
+        m_enabled = v;
         m_renderer.enabled = v;
     }
 }
