ecb5f23f0fdbb0d87cacbd0ad0e62957fc48895f
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-05-15 22:32:02 +0200

    Rewrite ShaderManager to LitMotion

--- Assets/Scripts/ShaderManager.cs
+++ Assets/Scripts/ShaderManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using LitMotion;
 using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -9,81 +10,60 @@ using UnityEngine.Rendering.Universal;
 public class ShaderManager : MonoBehaviour {
 	[SerializeField] private Material m_material;
 	[SerializeField] private float m_maxIntensity = 0.99f;
-	private float m_intensity;
-	private float m_currentIntensity;
 
-	[SerializeField] private AnimationCurve shaderCurveIn;
-	[SerializeField] private AnimationCurve shaderCurveOut;
-	[SerializeField] private float shaderDurationIn = 0.1f;
-	[SerializeField] private float shaderDurationOut = 1f;
-	private float m_shaderTime = 0f;
-	[SerializeField] private float testValue = 1f;
+	[SerializeField] private AnimationCurve m_shaderCurveIn;
+	[SerializeField] private AnimationCurve m_shaderCurveOut;
+	[SerializeField] private float m_shaderDurationIn = 0.1f;
+	[SerializeField] private float m_shaderDurationOut = 1f;
+	[SerializeField] private float m_testValue = 1f;
 
-	[SerializeField] private Volume UIPPVolume;
+	[SerializeField] private Volume m_uIPPVolume;
 	private SplitToning m_splitToning;
 	private float m_minSplitToning = 0f;
 	private float m_maxSplitToning = 100f;
 
 	[SerializeField] private UniversalRendererData m_universalRendererData;
-	private bool shaderOn = true;
+	private bool m_shaderOn = true;
 
 
 
 	[NaughtyAttributes.Button("Toggle Shader")]
 	public void ToggleShader() {
-		shaderOn = !shaderOn;
+		m_shaderOn = !m_shaderOn;
 		foreach (var feature in m_universalRendererData.rendererFeatures) {
-			feature.SetActive(shaderOn);
+			feature.SetActive(m_shaderOn);
 		}
-		UIPPVolume.profile.TryGet(out m_splitToning);
-		m_splitToning.active = shaderOn;
+		m_uIPPVolume.profile.TryGet(out m_splitToning);
+		m_splitToning.active = m_shaderOn;
 	}
 	private void Start() {
-		UIPPVolume.profile.TryGet(out m_splitToning);
+		m_uIPPVolume.profile.TryGet(out m_splitToning);
 		m_splitToning.balance.value = m_minSplitToning;
 	}
-	private bool m_fade;
 	// Start is called before the first frame update
 	[NaughtyAttributes.Button("Test Set Intensity")]
 	public void SetIntensity() {
-		SetIntensity(testValue);
+		SetIntensity(m_testValue);
 	}
 	private void SetIntensity(float intensity) {
-		m_intensity = intensity;
-		m_currentIntensity = intensity;
-		m_fade = true;
+		Debug.Log(intensity+"intensity");
+		LMotion.Create(0,intensity, m_shaderDurationIn).WithEase(m_shaderCurveIn).WithOnComplete(
+			()=>LMotion.Create(intensity, 0, m_shaderDurationOut).WithEase(m_shaderCurveOut).Bind(f=>m_material.SetFloat("_Intensity", f))
+		).Bind(f=>m_material.SetFloat("_Intensity", f));
 	}
 
 	//[NaughtyAttributes.Button("Set Split Toning")]
 	public void SetSplitToning() {
-		SetSplitToning(testValue);
+		SetSplitToning(m_testValue);
 	}
 	private void SetSplitToning(float ratio) {
 		m_splitToning.balance.value = Mathf.Lerp(m_minSplitToning, m_maxSplitToning, ratio);
 	}
 
-
-	// Update is called once per frame
-	private void Update() {
-
-		if (!m_fade) return;
-		if (m_shaderTime <= shaderDurationIn) {
-			m_currentIntensity = shaderCurveIn.Evaluate(m_shaderTime / shaderDurationIn) * m_intensity;
-		} else if (m_shaderTime <= shaderDurationOut + shaderDurationIn) {
-			m_currentIntensity = shaderCurveOut.Evaluate((m_shaderTime - shaderDurationIn) / shaderDurationOut) * m_intensity;
-		} else {
-			m_fade = false;
-			m_shaderTime = 0f;
-			m_currentIntensity = 0f;
-		}
-		m_material.SetFloat("_Intensity", m_currentIntensity);
-		m_shaderTime += Time.deltaTime;
-	}
-
 	//[Button("Hit")]
 	private void ProcessKillTest() {
 		float ratio = 0.6f;
-		SetIntensity(m_maxIntensity * ratio);
+		SetIntensity(m_maxIntensity);
 		SetSplitToning(ratio);
 	}
 
@@ -96,25 +76,24 @@ public class ShaderManager : MonoBehaviour {
 	}
 
 	private void Unready() {
-		m_shaderTime = 0f;
 		m_minSplitToning = 0f;
 		m_maxSplitToning = 100f;
-		shaderOn = true;
-		m_intensity = 0f;
-		m_currentIntensity = 0f;
-		m_material.SetFloat("_Intensity", m_currentIntensity);
+		m_shaderOn = true;
+		m_material.SetFloat("_Intensity", 0);
 	}
 
 	private void Ready() {
-		m_shaderTime = 0f;
 		m_minSplitToning = 0f;
 		m_maxSplitToning = 100f;
-		shaderOn = true;
-		m_intensity = 0f;
-		m_currentIntensity = 0f;
-		m_material.SetFloat("_Intensity", m_currentIntensity);
+		m_shaderOn = true;
+		m_material.SetFloat("_Intensity", 0);
 		Game.MainGameInfo.GlobalEvents.onChildEaten += ProcessKill;
 	}
 
-
+	private void OnApplicationQuit() {
+		m_minSplitToning = 0f;
+		m_maxSplitToning = 100f;
+		m_shaderOn = true;
+		m_material.SetFloat("_Intensity", 0);
+	}
 }
