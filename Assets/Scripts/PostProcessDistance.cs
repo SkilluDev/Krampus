fcd2573c34bfdf2a1b8a5d04995a38f9c52663e0
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-05-15 16:41:51 +0200

    delete unused comment

--- Assets/Scripts/PostProcessDistance.cs
+++ Assets/Scripts/PostProcessDistance.cs
@@ -39,27 +39,6 @@ public class PostProcessDistance : MonoBehaviour {
         if(!Game.Balling) return;
         m_distanceToClosest = Game.MainGameInfo.Krampus.ChildSensor.Dist;
         float deltaDistance = Mathf.Clamp01((m_minDistance - m_distanceToClosest) / m_minDistance);
-        /* if (m_distanceToClosest < m_minDistance) {
-            float deltaDistance = m_minDistance - m_distanceToClosest;
-            if (deltaDistance / m_minDistance <= m_vignetteIntensity) {
-                ChangeVignette(m_vignetteIntensity);
-            } else if (deltaDistance / m_minDistance > m_vignetteIntensity && deltaDistance / m_minDistance < m_maxVignette) {
-                ChangeVignette(deltaDistance / m_minDistance);
-            } else {
-                ChangeVignette(m_maxVignette);
-            }
-
-            if (deltaDistance / m_minDistance <= m_abberIntensity) {
-                ChangeAberration(m_vignetteIntensity);
-            } else if (deltaDistance / m_minDistance > m_abberIntensity && deltaDistance / m_minDistance < m_maxChroma) {
-                ChangeAberration(deltaDistance / m_minDistance);
-            } else {
-                ChangeAberration(m_maxChroma);
-            }
-        } else {
-            ResetVignetteToDefault(m_vignetteIntensity);
-            ResetAberrToDefault(m_abberIntensity);
-        } */
 
         m_vignette.intensity.value = math.remap(0,1, m_minVignetteIntensity, m_maxVignetteIntensity, deltaDistance);
         m_aberration.intensity.value = math.remap(0,1, m_minAbberIntensity, m_maxAbberIntensity, deltaDistance);
