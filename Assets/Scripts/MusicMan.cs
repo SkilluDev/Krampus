c85a2097e58c2a50afb0d9c27a92467ce3e75215
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-05-16 02:19:00 +0200

    Fixed music on start

--- Assets/Scripts/MusicMan.cs
+++ Assets/Scripts/MusicMan.cs
@@ -11,9 +11,15 @@ public class MusicMan : MonoBehaviour {
 	[SerializeField] private float m_fadeOutTime = 5f;
 
 	public float GameMusicLayer2Volume { get=>m_gameMusicLayer2.volume; set=>m_gameMusicLayer2.volume=value;}
+
+	private MotionHandle m_motionHandle1;
+	private MotionHandle m_motionHandle2;
 	public void Ready() {
+		if(m_motionHandle1.IsActive()) m_motionHandle1.Cancel();
+		if(m_motionHandle2.IsActive()) m_motionHandle2.Cancel();
 		switch (Game.CurrentState) {
 			case Game.State.MainGame:
+				m_gameMusicLayer2.volume = 1;
 				m_gameMusicLayer1.Play();
 				m_gameMusicLayer2.volume = 0;
 				m_gameMusicLayer2.Play();
@@ -25,9 +31,8 @@ public class MusicMan : MonoBehaviour {
 	}
 
 	public void StopMusic() {
-
-		LMotion.Create(m_gameMusicLayer1.volume,0,m_fadeOutTime).WithOnComplete(m_gameMusicLayer1.Stop).BindToVolume(m_gameMusicLayer1);
-		LMotion.Create(m_gameMusicLayer2.volume,0,m_fadeOutTime).WithOnComplete(m_gameMusicLayer2.Stop).BindToVolume(m_gameMusicLayer2);
+		m_motionHandle1 = LMotion.Create(m_gameMusicLayer1.volume,0,m_fadeOutTime).WithOnComplete(m_gameMusicLayer1.Stop).BindToVolume(m_gameMusicLayer1);
+		m_motionHandle2 = LMotion.Create(m_gameMusicLayer2.volume,0,m_fadeOutTime).WithOnComplete(m_gameMusicLayer2.Stop).BindToVolume(m_gameMusicLayer2);
 	}
 
 
