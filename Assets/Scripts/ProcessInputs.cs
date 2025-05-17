2a37f4e288330a155e1cd2facb77ba3ad4eb3b0c
bajtix <bajtix.b@gmail.com>
2025-05-17 11:44:39 +0200

    Misc: Inputs asset
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/ProcessInputs.cs
+++ Assets/Scripts/ProcessInputs.cs
@@ -1,16 +1,20 @@
 using System.Linq;
 using TMPro;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
+[ExecuteAlways]
 public class ProcessInputs : MonoBehaviour, ITextPreprocessor {
     private TMP_Text m_text;
 
     private void Awake() {
         m_text = GetComponent<TMP_Text>();
+        m_text.textPreprocessor = this;
     }
 
     private void Start() {
         m_text.textPreprocessor = this;
+        if (!Application.isPlaying) return;
         InputSubscribe.onChanged.AddListener(OnChanged);
     }
 
@@ -18,11 +22,42 @@ public class ProcessInputs : MonoBehaviour, ITextPreprocessor {
         m_text.SetText(m_text.text);
     }
 
-    public string PreprocessText(string text) {
-        var regex = new System.Text.RegularExpressions.Regex(@"<binding=""(.*?)"">");
-        var matches = regex.Matches(text);
+    private bool Check(string condition) {
+        if (string.IsNullOrWhiteSpace(condition)) return false;
+        string method = InputSubscribe.InputMethod.ToString();
+        string[] conditions = condition.Split(' ');
+        foreach (string c in conditions) {
+            if (c.StartsWith('!')) {
+                if (c.Contains(method)) return false;
+            } else {
+                if (!c.Contains(method)) return false;
+            }
+        }
+        return true;
+    }
 
-        foreach (System.Text.RegularExpressions.Match match in matches) {
+
+    public string PreprocessText(string text) {
+        var ifRegex = new Regex(@"<if (.*?)>(.*?)<\/if>");
+        var ifMatches = ifRegex.Matches(text);
+
+        foreach (Match ifMatch in ifMatches) {
+            if (ifMatch.Groups.Count > 2) {
+                string condition = ifMatch.Groups[1].Value;
+                string content = ifMatch.Groups[2].Value;
+
+                if (Check(condition)) {
+                    text = text.Replace(ifMatch.Value, content);
+                } else {
+                    text = text.Replace(ifMatch.Value, string.Empty);
+                }
+            }
+        }
+
+        var bindingRegex = new Regex(@"<binding=""(.*?)"">");
+        var bindingMatches = bindingRegex.Matches(text);
+
+        foreach (Match match in bindingMatches) {
             if (match.Groups.Count > 1) {
                 string actionName = match.Groups[1].Value;
                 var action = InputSubscribe.Raw.asset.FindAction(actionName);
@@ -36,13 +71,6 @@ public class ProcessInputs : MonoBehaviour, ITextPreprocessor {
             }
         }
         return text;
-        //j
-        //k
-        //l
-        //m
-        //n
-        //o
-        //p
     }
 
 }
