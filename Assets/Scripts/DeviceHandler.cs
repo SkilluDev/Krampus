fc2d1a14c76d1e811013a497f42798baf1ec79c8
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-05-18 01:58:35 +0200

    cursor visibility toggle on input change

--- Assets/Scripts/DeviceHandler.cs
+++ Assets/Scripts/DeviceHandler.cs
@@ -15,25 +15,15 @@ public class DeviceHandler : MonoBehaviour {
         m_listener.Dispose();
     }
 
-    private void OnControlsChanged(PlayerInput playerInput) {
-        string deviceClass = playerInput.currentControlScheme;
-
-        if (deviceClass.Equals("PC")) {
-            InputSubscribe.ChangeInputMethod(InputSubscribe.Method.PC);
-        } else {
-            InputSubscribe.ChangeInputMethod(InputSubscribe.Method.Console);
-        }
-
-        Debug.Log("ON CONTROLS CHANGED");
-    }
-
     private void OnButtonPressed(InputControl button) {
         string deviceClass = button.device.description.deviceClass;
 
         if (deviceClass.Equals("Keyboard") || deviceClass.Equals("Mouse")) {
             InputSubscribe.ChangeInputMethod(InputSubscribe.Method.PC);
+            Cursor.visible = true;
         } else {
             InputSubscribe.ChangeInputMethod(InputSubscribe.Method.Console);
+            Cursor.visible = false;
         }
 
         // Optional: Log the device that triggered the switch
