fad9014b8f024b3d389e185a0f0b9dd185261324
bajtix <bajtix.b@gmail.com>
2025-05-17 23:47:06 +0200

    outro changes
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/OutroScript.cs
+++ Assets/Scripts/OutroScript.cs
@@ -6,28 +6,27 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class OutroScript : MonoBehaviour
-{
-   [SerializeField] Animator m_animator;
-   [SerializeField] PlayableDirector m_director;
-   [SerializeField] LineRenderer m_tongue;
-   [SerializeField] private Transform jawOrigin;
-   [SerializeField] private Transform jawTarget;
+public class OutroScript : MonoBehaviour {
+	[SerializeField] Animator m_animator;
+	[SerializeField] PlayableDirector m_director;
+	[SerializeField] LineRenderer m_tongue;
+	[SerializeField] private Transform jawOrigin;
+	[SerializeField] private Transform jawTarget;
 	bool isPlaying = false;
 
 
 	private void Update() {
-		if(!isPlaying) return;
+		if (!isPlaying) return;
 		m_tongue.SetPosition(0, jawOrigin.position);
 		m_tongue.SetPosition(1, jawTarget.position);
 	}
 
 	[Button("WIN")]
-   public void PlayOutro() {
-	   m_director.Play();
-	   transform.position = Game.MainGameInfo.Krampus.transform.position;
-	   Game.MainGameInfo.Krampus.gameObject.SetActive(false);
-	   m_animator.SetTrigger("Outro");
-	   isPlaying = true;
-   }
+	public void PlayOutro() {
+		m_director.Play();
+		transform.position = Game.MainGameInfo.Krampus.transform.position;
+		Game.MainGameInfo.Krampus.Animator.SetEnableModel(false);
+		//m_animator.SetTrigger("Outro");
+		isPlaying = true;
+	}
 }
