70d98124c73985022ea3ecdce18963d1a53d9def
Akkokiko <Maciek.Polubiec@hotmail.com>
2025-06-01 22:37:51 +0200

    Item pool

---
+++ Assets/Scripts/Items/ItemPool.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPool : ScriptableObject {
+    public Item[] items;
+
+
+    public Item[] getItem(int size) {
+        Item[] results = new Item[size];
+
+        return results;
+     }
+    
+
+}
