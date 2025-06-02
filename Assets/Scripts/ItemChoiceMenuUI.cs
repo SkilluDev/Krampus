783468fcff9ef976d0fdc9a683a027f751303883
bajtix <bajtix.b@gmail.com>
2025-06-02 19:31:42 +0200

    Misc: fix item choice menu ui
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/ItemChoiceMenuUI.cs
+++ Assets/Scripts/ItemChoiceMenuUI.cs
@@ -3,41 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemChoiceMenuUI : MonoBehaviour {
-     [SerializeField] private ItemCard[] m_itemCards;
+    [SerializeField] private ItemCard[] m_itemCards;
 
-    public Item[] m_itemsDisplayed;
+    private Item[] m_itemsDisplayed;
 
 
-	public void OnEnable() {
+    public void OnEnable() {
         SetItems();
-	}
+    }
 
-	public void SetItems() {
-
-        Item[] items = Game.MainGameInfo.ItemPool.RandomItemFor(m_itemCards.Length, Game.MainGameInfo.Krampus.Stats);
-
+    public void SetItems() {
+        var items = Game.MainGameInfo.ItemPool.RandomItemFor(m_itemCards.Length, Game.MainGameInfo.Krampus.Stats);
         m_itemsDisplayed = new Item[m_itemCards.Length];
-
         for (int i = 0; i < m_itemCards.Length; i++) {
-
             var a = items[i];
             m_itemsDisplayed[i] = a;
             m_itemCards[i].SetInfo(a.ItemIcon, a.ItemName, a.Description);
         }
-
     }
 
     public void ChooseItem(int pos) {
-
-        Game.PogMan.AddItem(m_itemsDisplayed[pos]);
-        
+        Game.MainGameInfo.Krampus.Stats.AddItem(m_itemsDisplayed[pos]);
 
         Game.MainGameInfo.SetState(MainGameInfo.State.Game);
         Game.MainGameInfo.UI.UIElementsEntryAnimation();
 
-        this.gameObject.SetActive(false);
+        gameObject.SetActive(false);
 
-        Debug.Log("Zyskano item: " + m_itemsDisplayed[pos].ItemName);
+        Debug.Log("Got item: " + m_itemsDisplayed[pos].ItemName);
     }
 
 
