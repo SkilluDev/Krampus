d6daaf494fb88da19b3e6cf7c78261bd5c0e2a16
bajtix <bajtix.b@gmail.com>
2025-06-02 19:07:52 +0200

    Items: sort out the item situation
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,37 +1,46 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [CreateAssetMenu(menuName = "Game/Items/Item", fileName = "Item")]
 public class Item : ScriptableObject {
-
     [SerializeField] protected string m_itemName = "Item Name";
     public string ItemName => m_itemName;
 
-
     [SerializeField] private Sprite m_itemIcon;
     public Sprite ItemIcon => m_itemIcon;
-
 
     [TextArea(4, 25)]
     [SerializeField] private string m_description;
     public string Description => m_description;
 
-    protected Krampus m_krampus;
+    /// <summary>
+    /// Caled when this Item's events should be registered
+    /// </summary>
+    /// <param name="krampus"></param>
+    public virtual void RegisterEvents(Krampus krampus) {
 
-
-    virtual public void RegisterItem(Krampus krampus) {
-
-        m_krampus = krampus;
     }
+    /// <summary>
+    /// Called when this Item's events should be unregistered
+    /// </summary>
+    /// <param name="krampus"></param>
+    public virtual void UnregisterEvents(Krampus krampus) {
 
-
-    public bool IsItem(Item item) {
-        return this.m_itemName.Equals(item.m_itemName);
-     }
-
-
-
+    }
+    /// <summary>
+    /// Called while evaluating modifiers 
+    /// </summary>
+    /// <returns>The list of modifiers which should be currently applied to Krampus</returns>
+    public virtual IEnumerable<StatModifier> GetModifiers(Krampus krampus) {
+        return Array.Empty<StatModifier>();
+    }
+    /// <summary>
+    /// Called when this item gets added to a Krampus 
+    /// </summary>
+    public virtual void ItemAdded(Krampus krampus) { }
+    /// <summary>
+    /// Called when this item gets removed from a Krampus
+    /// </summary>
+    public virtual void ItemRemoved(Krampus krampus) { }
 }
