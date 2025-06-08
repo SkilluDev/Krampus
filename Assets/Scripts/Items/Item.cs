19f9fb12ea493fa0c28762c6a106453f958682e2
s32087 <s32087@pjwstk.edu.pl>
2025-06-08 11:05:10 +0200

    Item system change - effect list on items, dynamic id

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -4,41 +4,68 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game/Items/Item", fileName = "Item")]
 public class Item : ScriptableObject {
-    [SerializeField] protected string m_itemName = "Item Name";
-    public string ItemName => m_itemName;
+	[SerializeField] protected string m_itemName = "Item Name";
+	public string ItemName => m_itemName;
 
-    [SerializeField] private Sprite m_itemIcon;
-    public Sprite ItemIcon => m_itemIcon;
+	[SerializeField] private Sprite m_itemIcon;
+	public Sprite ItemIcon => m_itemIcon;
 
-    [TextArea(4, 25)]
-    [SerializeField] private string m_description;
-    public string Description => m_description;
+	[TextArea(4, 25)]
+	[SerializeField] private string m_description;
+	public string Description => m_description;
 
-    /// <summary>
-    /// Caled when this Item's events should be registered
-    /// </summary>
-    public virtual void RegisterEvents(KrampusEvents events) {
+	[SerializeField] private List<EffectInEditor> m_effectsInEditor = new List<EffectInEditor>();
 
-    }
-    /// <summary>
-    /// Called when this Item's events should be unregistered
-    /// </summary>
-    public virtual void UnregisterEvents(KrampusEvents events) {
+	protected List<Effect> m_effects = new List<Effect>();
 
-    }
-    /// <summary>
-    /// Called while evaluating modifiers 
-    /// </summary>
-    /// <returns>The list of modifiers which should be currently applied to Krampus</returns>
-    public virtual IEnumerable<StatModifier> GetModifiers(Krampus krampus) {
-        return Array.Empty<StatModifier>();
-    }
-    /// <summary>
-    /// Called when this item gets added to a Krampus 
-    /// </summary>
-    public virtual void ItemAdded(Krampus krampus) { }
-    /// <summary>
-    /// Called when this item gets removed from a Krampus
-    /// </summary>
-    public virtual void ItemRemoved(Krampus krampus) { }
+
+
+	/// <summary>
+	/// Caled when this Item's events should be registered
+	/// </summary>
+	public virtual void RegisterEvents(KrampusEvents events) {
+
+	}
+	/// <summary>
+	/// Called when this Item's events should be unregistered
+	/// </summary>
+	public virtual void UnregisterEvents(KrampusEvents events) {
+
+	}
+	/// <summary>
+	/// Called while evaluating modifiers
+	/// </summary>
+	/// <returns>The list of modifiers which should be currently applied to Krampus</returns>
+	public virtual IEnumerable<StatModifier> GetModifiers(Krampus krampus) {
+		return Array.Empty<StatModifier>();
+	}
+	/// <summary>
+	/// Called when this item gets added to a Krampus
+	/// </summary>
+	public virtual void ItemAdded(Krampus krampus) {
+		foreach (var e in m_effectsInEditor) {
+			m_effects.Add(e.ToEffect(ItemName));
+		}
+
+	}
+	/// <summary>
+	/// Called when this item gets removed from a Krampus
+	/// </summary>
+	public virtual void ItemRemoved(Krampus krampus) {
+		foreach (var e in m_effectsInEditor) {
+			m_effects.Remove(e.ToEffect(ItemName));
+		}
+	}
+
+	public virtual void RegisterAllEffects(Krampus krampus) {
+		foreach (var effect in m_effects) {
+			krampus.Stats.RegisterEffect(effect);
+		}
+	}
+
+	public virtual void UnregisterAllEffects(Krampus krampus) {
+		foreach (var effect in m_effects) {
+			krampus.Stats.UnregisterEffect(effect);
+		}
+	}
 }
--- Assets/Scripts/Modifiers/Effect.cs
+++ Assets/Scripts/Modifiers/Effect.cs
@@ -1,46 +1,65 @@
-public class Effect {
-    public enum Type {
-        Permanent,
-        Temporary
-        
-    }
+using System;
+using NaughtyAttributes;
+using UnityEngine;
 
-    public string m_id;
+[Serializable]
+public class EffectInEditor {
+	[SerializeField] private KrampusStats.Stat m_stat;
+	[SerializeField] private float m_duration;
+	[SerializeField] private float m_modifier;
 
-    private Type m_type;
-    public Type EffectType => m_type;
+	public Effect ToEffect(string itemName) {
+		if (m_duration > 0) {
+			return new Effect(m_stat, m_modifier, itemName+"_"+m_stat);
+		}
+		return new Effect(m_stat, m_modifier, m_duration, itemName+"_"+m_stat);
 
-
-
-    private bool m_isExpired = false;
-    public bool IsExpired { get => m_isExpired; set => m_isExpired = value; }
-    protected float m_timer;
-
-    private StatModifier m_statModifier;
-
-    public StatModifier StatModifier => m_statModifier;
-
-
-
-    public Effect(KrampusStats.Stat stat, float modifier, string id) {
-        m_statModifier = new StatModifier(stat, modifier);
-        m_type = Type.Permanent;
-        m_id = id;
-    }
-
-    public Effect(KrampusStats.Stat stat, float modifier, float duration,string id) {
-        m_statModifier = m_statModifier = new StatModifier(stat, modifier);
-        m_timer = duration;
-        m_type = Type.Temporary;
-        m_id = id;
-    }
-
-    public void UpdateTimer(float deltaTime) {
-        if (m_timer > 0) {
-            m_timer -= deltaTime;
-        } else {
-            m_isExpired = true;
-        }
-    }
-    
+	}
 }
+
+public class Effect {
+		public enum Type {
+			Permanent,
+			Temporary
+
+		}
+
+		public string m_id;
+
+		private Type m_type;
+		public Type EffectType => m_type;
+
+
+
+		private bool m_isExpired = false;
+		public bool IsExpired { get => m_isExpired; set => m_isExpired = value; }
+		protected float m_timer;
+
+		private StatModifier m_statModifier;
+
+		public StatModifier StatModifier => m_statModifier;
+
+
+
+		public Effect(KrampusStats.Stat stat, float modifier, string id) {
+			m_statModifier = new StatModifier(stat, modifier);
+			m_type = Type.Permanent;
+			m_id = id;
+		}
+
+		public Effect(KrampusStats.Stat stat, float modifier, float duration, string id) {
+			m_statModifier = m_statModifier = new StatModifier(stat, modifier);
+			m_timer = duration;
+			m_type = Type.Temporary;
+			m_id = id;
+		}
+
+		public void UpdateTimer(float deltaTime) {
+			if (m_timer > 0) {
+				m_timer -= deltaTime;
+			} else {
+				m_isExpired = true;
+			}
+		}
+
+	}
