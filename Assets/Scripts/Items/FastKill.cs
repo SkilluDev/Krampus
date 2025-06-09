b5183b0fca14bc3e1d321247e2fe284cba0d7307
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-06-09 02:27:58 +0200

    Revert some

--- Assets/Scripts/Items/FastKill.cs
+++ Assets/Scripts/Items/FastKill.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game/Items/FastKill", fileName = "FastKill")]
 public class FastKill : Item {
 
-    public override void RegisterEvents(KrampusEvents events) {
+	public override void RegisterEvents(KrampusEvents events) {
 		events.onNaughtyChildEaten.AddListener(BuffKrampus);
 		events.onNiceChildEaten.AddListener(BuffKrampus);
 	}
 
-    public override void UnregisterEvents(KrampusEvents events) {
-        events.onNaughtyChildEaten.RemoveListener(BuffKrampus);
-        events.onNiceChildEaten.RemoveListener(BuffKrampus);
-    }
+	public override void UnregisterEvents(KrampusEvents events) {
+		events.onNaughtyChildEaten.RemoveListener(BuffKrampus);
+		events.onNiceChildEaten.RemoveListener(BuffKrampus);
+	}
+
+	private void BuffKrampus(Krampus krampus, Child child) {
+		//Game.MainGameInfo.UI.DisplayEffect(m_duration, ItemIcon, ItemName,m_effectId);
+		RegisterAllEffects(krampus);
+	}
 }
