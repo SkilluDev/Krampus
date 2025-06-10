a26c85b681bf2d4074bbf26ef1aac0dde6e7003e
Akkokiko <Maciek.Polubiec@hotmail.com>
2025-06-10 12:27:04 +0200

    NunMissleVisuals

--- Assets/NunMissle.cs
+++ Assets/NunMissle.cs
@@ -9,12 +9,18 @@ public class NunMissle : MonoBehaviour {
     [SerializeField] private float m_rotationSpeed = 50;
 
     [SerializeField] private Rigidbody m_rigidbody;
+    [SerializeField] private Transform m_visuals;
+    [SerializeField] private GameObject m_missleMesh;
 
     private Vector3 m_direction;
+    private bool m_isActive = true;
 
 
     private void Update() {
         if (m_target == null) return;
+        if (m_isActive == false) {
+            return;
+        }
 
 
 
@@ -24,18 +30,28 @@ public class NunMissle : MonoBehaviour {
     }
 
     public void SetTarget(Transform target, Vector3 direction) {
+        m_isActive = true;
         m_target = target;
         m_direction = direction;
+        m_visuals.rotation = Quaternion.LookRotation(direction);
     }
 
     void OnTriggerEnter(Collider other) {
+        if (m_isActive == false) return;
         if (other.gameObject.layer == LayerMask.NameToLayer("Wall")) {
-            Destroy(gameObject);
+            DesactivateMissle();
+           
         }
         if (other.gameObject.tag == "Player") {
             KrampusStats krampus = other.GetComponent<KrampusStats>();
             krampus.RegisterEffect(new Effect(KrampusStats.Stat.Speed, -0.3f, 3, "Nun_Slow", null));
-            Destroy(gameObject);
-         }
-	}
+            DesactivateMissle();
+        }
+    }
+
+    void DesactivateMissle() {
+        m_isActive = false;
+        m_missleMesh.SetActive(false);
+         Destroy(gameObject, 2);
+     }
 }
