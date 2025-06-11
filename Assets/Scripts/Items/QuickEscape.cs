73c733bf1baaf467d3e336a66450d5e914af0a93
Akkokiko <Maciek.Polubiec@hotmail.com>
2025-06-11 10:36:00 +0200

    New Item: JOJO

---
+++ Assets/Scripts/Items/QuickEscape.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Game/Items/Jojo", fileName = "JOJO")]
+public class QuickEscape : Item {
+	public override void RegisterEvents(KrampusEvents events) {
+		events.onLockOut.AddListener(BuffKrampus);
+
+	}
+
+	public override void UnregisterEvents(KrampusEvents events) {
+		events.onLockOut.RemoveListener(BuffKrampus);
+	}
+
+	private void BuffKrampus(Krampus krampus) {
+		Debug.Log("LOCKEDTONGUE");
+		RegisterAllEffects(krampus);
+	}
+
+}
