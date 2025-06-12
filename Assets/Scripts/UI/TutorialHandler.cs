cd32a7a9c0aa1da37e8e897e39d34059065b5b96
IgorSienkiewicz <147891614+SkilluDev@users.noreply.github.com>
2025-06-12 21:25:17 +0200

    Fix tutorial bug

--- Assets/Scripts/UI/TutorialHandler.cs
+++ Assets/Scripts/UI/TutorialHandler.cs
@@ -77,7 +77,10 @@ public class TutorialHandler : MonoBehaviour {
 	}
 
 	private void OnTutorialTrigger(TutorialPage pages) {
-		if (!Game.SetMan.GetValue<bool>("Show Tutorial")) return;
+		if (!Game.SetMan.GetValue<bool>("Show Tutorial")) {
+			Game.MainGameInfo.SetState(MainGameInfo.State.ItemChoosing);
+			return;
+		}
 		if (pages == 0) return;
 		gameObject.SetActive(true);
 		PrepareTutorials(pages);
