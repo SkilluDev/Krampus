7356ba238f8c848fbbee14b0802141746f20f320
Akkokiko <Maciek.Polubiec@hotmail.com>
2025-06-17 07:17:13 +0200

    Box New VisualEffect

--- Assets/Scripts/TongueInteraction/Box.cs
+++ Assets/Scripts/TongueInteraction/Box.cs
@@ -10,13 +10,15 @@ public class Box : MonoBehaviour, IThrowable {
     
     [SerializeField] private LayerMask m_destroyMask;
     [SerializeField] private LayerMask m_stunMask;
-    [SerializeField] private Transform m_model;
+    [SerializeField] private Transform m_allModel;
+     [SerializeField] private Transform m_boxModel;
     [SerializeField] private GameObject m_specialEffect;
 
     [SerializeField] private float m_stunDuration;
 
     [SerializeField] private Collider m_collider;
     [SerializeField] private Rigidbody m_rigidbody;
+    [SerializeField] private bool m_inMove;
    private Krampus m_owner;
 
     [SerializeField] private float m_throwForce;
@@ -43,22 +45,28 @@ public class Box : MonoBehaviour, IThrowable {
 
 
     }
+	private void Update() {
+        if (m_inMove) {
+            m_boxModel.Rotate(1440 * Time.deltaTime, 0, 0);
+        }
+	}
 
-    public void ReelIn(Krampus krampus, Vector3 position, float progress) {
+	public void ReelIn(Krampus krampus, Vector3 position, float progress) {
         transform.position = position - transform.InverseTransformPoint(m_pinTarget.position);
-        
+
     }
 
     public void Throw(Vector3 vector3, Krampus krampus) {
 
         transform.rotation = Quaternion.identity;
-        m_model.rotation= Quaternion.LookRotation(vector3);
+        m_allModel.rotation = Quaternion.LookRotation(vector3);
         m_specialEffect.SetActive(true);
         m_rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
         m_rigidbody.velocity = vector3 * m_throwForce;
         m_collider.enabled = true;
         m_collider.isTrigger = true;
         m_owner = krampus;
+        m_inMove = true;
 
     }
     
