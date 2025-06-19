0aa5e94dde5682c870c61fe3c576ebe0bdfaf4b3
bajtix <bajtix.b@gmail.com>
2025-06-19 14:27:37 +0200

    R: refactor ItemPool
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Items/ItemPool.cs
+++ Assets/Scripts/Items/ItemPool.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
+using KrampUtils;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game/Items/ItemPool", fileName = "ItemPool")]
 public class ItemPool : ScriptableObject {
     public Item[] items;
 
-
     public Item[] RandomItemFor(int size, KrampusStats krampusStats) {
 
-        List<int> indexes = new List<int>();
+        var indexes = new List<int>();
         for (int i = 0; i < items.Length; i++) {
             Item item = items[i];
             //Debug.Log("Checking " + item.ItemName);
@@ -18,8 +18,8 @@ public class ItemPool : ScriptableObject {
             }
         }
 
-        int[] pos =  Shuffle(indexes.ToArray());
-        Item[] results = new Item[size];
+        int[] pos = indexes.UnityShuffle();
+        var results = new Item[size];
 
         for (int j = 0; j < size; j++) {
             results[j] = items[pos[j]];
@@ -28,18 +28,6 @@ public class ItemPool : ScriptableObject {
         return results;
     }
 
-
-    private int[] Shuffle(int[] el) {
-        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
-        for (int t = 0; t < el.Length; t++) {
-            int tmp = el[t];
-            int r = UnityEngine.Random.Range(t, el.Length);
-            el[t] = el[r];
-            el[r] = tmp;
-        }
-
-        return el;
-    }
 }
 
 
