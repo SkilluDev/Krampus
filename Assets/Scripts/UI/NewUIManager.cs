aaecaef4635cf919ae5d991eb68286d698a7b6e7
SkilluDev <147891614+SkilluDev@users.noreply.github.com>
2025-06-21 16:08:30 +0200

    Cursor change

--- Assets/Scripts/UI/NewUIManager.cs
+++ Assets/Scripts/UI/NewUIManager.cs
@@ -198,8 +198,8 @@ public class NewUIManager : MonoBehaviour {
     private void FixedUpdate() {
         Vector3 mousePos;
         mousePos = Mouse.current.position.ReadValue();
-        m_quickActionIcon.transform.position = mousePos + new Vector3(-1f, 17f, 0f);
-       
+        m_quickActionIcon.transform.position = mousePos + new Vector3(-1f, 37f, 0f);
+
 	}
 
 	public void SetChildrenIcon(Sprite icon) {
