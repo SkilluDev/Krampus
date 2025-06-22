03ad99b8866a9f5a4c2e9faf1a82340a87256993
bajtix <bajtix.b@gmail.com>
2025-06-22 12:24:21 +0200

    Misc: add guns
    Signed-off-by: bajtix <bajtix.b@gmail.com>

---
+++ Assets/TestGun.cs
@@ -0,0 +1,23 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class TestGun : MonoBehaviour {
+    [SerializeField] private Projectile m_projectile;
+    [SerializeField] private float m_interval = 4;
+    [SerializeField] private Transform m_target;
+
+    private float m_countdown;
+
+    private void Start() {
+        m_countdown = m_interval;
+    }
+
+    private void Update() {
+        if (m_countdown <= 0) {
+            Projectile.Shoot(m_projectile, transform.position, transform.forward, m_target);
+            m_countdown = m_interval;
+        }
+        m_countdown -= Time.deltaTime;
+    }
+}
+
