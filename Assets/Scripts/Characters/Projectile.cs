d1f43a366a4b73d04a6377e01d446cc177a03b4b
bajtix <bajtix.b@gmail.com>
2025-06-22 23:09:41 +0200

    VFX: Nun fireball shoot vfx
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Characters/Projectile.cs
+++ Assets/Scripts/Characters/Projectile.cs
@@ -3,23 +3,29 @@ using NaughtyAttributes;
 using Sound;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
 
 public abstract class Projectile : MonoBehaviour {
-    [BoxGroup("Components")][SerializeField] protected Rigidbody m_rigidbody;
-    [BoxGroup("Components")][SerializeField] protected AudioSource m_audioSource;
     [SerializeField] protected float m_speed = 14;
     [SerializeField] protected float m_turningSpeed = 15;
+    [SerializeField] protected LayerMask m_destroyedBy;
+    [SerializeField] protected Tag m_targetGroup;
 
+    [BoxGroup("Components")][SerializeField] protected Rigidbody m_rigidbody;
+    [BoxGroup("Components")][SerializeField] protected AudioSource m_audioSource;
+    [BoxGroup("Components")][SerializeField] protected Animator m_animator;
     [BoxGroup("Sounds")][SerializeField] protected Sex m_sexShoot;
     [BoxGroup("Sounds")][SerializeField] protected Sex m_sexHit;
     [BoxGroup("Sounds")][SerializeField] protected Sex m_sexMiss;
-    [BoxGroup("Effects")][SerializeField] protected GameObject m_efHit;
-    [BoxGroup("Effects")][SerializeField] protected GameObject m_efMiss;
+    [BoxGroup("Effects")][SerializeField] protected GameObject m_fxShoot;
+    [BoxGroup("Effects")][SerializeField] protected GameObject m_fxHit;
+    [BoxGroup("Effects")][SerializeField] protected GameObject m_fxMiss;
+    [ShowIf("HasAnimator")][BoxGroup("Animations")][SerializeField][AnimatorParam("m_animator")] protected int m_animShoot;
+    [ShowIf("HasAnimator")][BoxGroup("Animations")][SerializeField][AnimatorParam("m_animator")] protected int m_animHit;
+    [ShowIf("HasAnimator")][BoxGroup("Animations")][SerializeField][AnimatorParam("m_animator")] protected int m_animMiss;
 
-
-    [SerializeField] protected LayerMask m_destroyedBy;
-    [SerializeField] protected Tag m_targetGroup;
+    protected bool HasAnimator => m_animator != null;
+    protected bool HasAudioSource => m_audioSource != null;
 
     protected Transform m_target;
 
@@ -34,17 +40,21 @@ public abstract class Projectile : MonoBehaviour {
     protected virtual void Shoot(Transform target) {
         m_target = target;
         m_sexShoot.Play(transform.position);
+        if (m_fxShoot != null) Instantiate(m_fxShoot, transform.position, transform.rotation);
+        if (m_animator != null) m_animator.SetTrigger(m_animShoot);
     }
 
     protected virtual void Miss(Collider other) {
         m_sexMiss.Play(transform.position);
-        if (m_efMiss != null) Instantiate(m_efMiss, transform.position, transform.rotation);
+        if (m_fxMiss != null) Instantiate(m_fxMiss, transform.position, transform.rotation);
+        if (m_animator != null) m_animator.SetTrigger(m_animMiss);
         Destroy(gameObject);
     }
 
     protected virtual void Hit(Collider other) {
         m_sexHit.Play(transform.position);
-        if (m_efHit != null) Instantiate(m_efHit, transform.position, transform.rotation);
+        if (m_fxHit != null) Instantiate(m_fxHit, transform.position, transform.rotation);
+        if (m_animator != null) m_animator.SetTrigger(m_animHit);
         Destroy(gameObject);
     }
 
