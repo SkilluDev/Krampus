f43b8114da38953a26997387c630ea15ea6a3e20
bajtix <bajtix.b@gmail.com>
2025-06-23 13:33:10 +0200

    R: Nun missiles use the new projectile system
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Characters/Nun/NunMissile.cs
+++ Assets/Scripts/Characters/Nun/NunMissile.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
 public class NunMissile : Projectile {
-    protected override void Hit(Collider other) => base.Hit(other);
+    [SerializeField] private EffectInEditor m_effectToAdd;
+    [SerializeField] private Sprite m_slowEffectIcon;
+    protected override void Hit(Collider other) {
+        base.Hit(other);
+        var krampus = other.GetComponent<Krampus>().Stats;
+        krampus.RegisterEffect(m_effectToAdd.ToEffect("Nun_Slow", m_slowEffectIcon));
+    }
 
 }
