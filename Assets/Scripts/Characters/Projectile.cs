0775f1881d877ca69d834c458ad712101d8a4f96
bajtix <bajtix.b@gmail.com>
2025-06-23 12:08:11 +0200

    VFX: NunMissileMiss
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- Assets/Scripts/Characters/Projectile.cs
+++ Assets/Scripts/Characters/Projectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 
-public abstract class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour {
     [SerializeField] protected float m_speed = 14;
     [SerializeField] protected float m_turningSpeed = 15;
     [SerializeField] protected LayerMask m_destroyedBy;
@@ -20,6 +20,12 @@ public abstract class Projectile : MonoBehaviour {
     [BoxGroup("Effects")][SerializeField] protected GameObject m_fxShoot;
     [BoxGroup("Effects")][SerializeField] protected GameObject m_fxHit;
     [BoxGroup("Effects")][SerializeField] protected GameObject m_fxMiss;
+    [BoxGroup("Auto Destroy")][SerializeField] protected bool m_dieOnMiss;
+    [BoxGroup("Auto Destroy")][EnableIf("m_dieOnMiss")][SerializeField] protected float m_timeMiss;
+    [BoxGroup("Auto Destroy")][SerializeField] protected bool m_dieOnHit;
+    [BoxGroup("Auto Destroy")][EnableIf("m_dieOnHit")][SerializeField] protected float m_timeHit;
+
+
     [ShowIf("HasAnimator")][BoxGroup("Animations")][SerializeField][AnimatorParam("m_animator")] protected int m_animShoot;
     [ShowIf("HasAnimator")][BoxGroup("Animations")][SerializeField][AnimatorParam("m_animator")] protected int m_animHit;
     [ShowIf("HasAnimator")][BoxGroup("Animations")][SerializeField][AnimatorParam("m_animator")] protected int m_animMiss;
@@ -27,6 +33,7 @@ public abstract class Projectile : MonoBehaviour {
     protected bool HasAnimator => m_animator != null;
     protected bool HasAudioSource => m_audioSource != null;
 
+    protected bool m_isActive;
     protected Transform m_target;
 
     public static T Shoot<T>(T projectile, Vector3 position, Vector3 direction, Transform target = null) where T : Projectile {
@@ -42,23 +49,32 @@ public abstract class Projectile : MonoBehaviour {
         m_sexShoot.Play(transform.position);
         if (m_fxShoot != null) Instantiate(m_fxShoot, transform.position, transform.rotation);
         if (m_animator != null) m_animator.SetTrigger(m_animShoot);
+        m_isActive = true;
     }
 
     protected virtual void Miss(Collider other) {
         m_sexMiss.Play(transform.position);
         if (m_fxMiss != null) Instantiate(m_fxMiss, transform.position, transform.rotation);
         if (m_animator != null) m_animator.SetTrigger(m_animMiss);
-        Destroy(gameObject);
+        m_isActive = false;
+        if (m_dieOnMiss) Destroy(gameObject, m_timeMiss);
     }
 
     protected virtual void Hit(Collider other) {
         m_sexHit.Play(transform.position);
         if (m_fxHit != null) Instantiate(m_fxHit, transform.position, transform.rotation);
         if (m_animator != null) m_animator.SetTrigger(m_animHit);
-        Destroy(gameObject);
+        m_isActive = false;
+        if (m_dieOnHit) Destroy(gameObject, m_timeHit);
     }
 
     protected virtual void FixedUpdate() {
+        if (!m_isActive) {
+            m_rigidbody.velocity = Vector3.zero;
+            Debug.Log("stop!");
+            return;
+        }
+
         m_rigidbody.velocity = transform.forward * m_speed;
 
         if (m_target != null) {
@@ -74,6 +90,7 @@ public abstract class Projectile : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider other) {
+        if (!m_isActive) return;
         if (m_destroyedBy.IsLayerMasked(other.gameObject.layer)) {
             Miss(other);
         }
