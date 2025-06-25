af2a43ac6697f4b50805076bbbbf183e847bd31b
Akkokiko <Maciek.Polubiec@hotmail.com>
2025-06-25 10:39:33 +0200

    Nun missle ignores open door

--- Assets/Scripts/Characters/Nun/NunMissile.cs
+++ Assets/Scripts/Characters/Nun/NunMissile.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class NunMissile : Projectile {
     [SerializeField] private EffectInEditor m_effectToAdd;
+    [SerializeField] private Tag m_doorTag;
     [SerializeField] private Sprite m_slowEffectIcon;
     protected override void Hit(Collider other) {
         base.Hit(other);
         var krampus = other.GetComponent<Krampus>().Stats;
         krampus.RegisterEffect(m_effectToAdd.ToEffect("Nun_Slow", m_slowEffectIcon));
     }
+
+    protected override void Miss(Collider other) {
+        if (other.gameObject.HasTag(m_doorTag)) {
+            if (!other.gameObject.GetComponent<Door>().IsOpen) {
+                base.Miss(other);
+             }
+           
+
+           
+        } else
+            base.Miss(other);
+    }
+
 
 }
