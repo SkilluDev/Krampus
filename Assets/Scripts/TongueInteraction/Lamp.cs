2ab13a048cf69c223da58278413d3451fb32d903
bajtix <bajtix.b@gmail.com>
2026-06-20 18:49:45 +0200

    fix lighting

--- Assets/Scripts/TongueInteraction/Lamp.cs
+++ Assets/Scripts/TongueInteraction/Lamp.cs
@@ -1,27 +1,33 @@
+using SaintsField.Playa;
 using UnityEngine;
 
 public class Lamp : MonoBehaviour, IInteractable {
     [SerializeField] private Transform m_switch;
-    [SerializeField] private Light m_light;
+    [SerializeField] private Light[] m_lights;
     [SerializeField] private MeshRenderer m_renderer;
     [SerializeField] private bool m_enabledByDefault = true;
+    [ShowInInspector] public bool IsOn { get; private set; }
     public Vector3 InteractionPoint => m_switch != null ? m_switch.position : transform.position;
 
     public int Priority => -15;
 
     private void Start() {
-        if (!m_enabledByDefault) Toggle();
+        Toggle(m_enabledByDefault);
     }
 
-    public void Toggle() {
-        m_light.enabled = !m_light.enabled;
-        if (m_light.enabled)
+    public void Toggle(bool newState) {
+        IsOn = newState;
+        foreach (var light in m_lights) {
+            light.enabled = IsOn;
+        }
+
+        if (IsOn)
             m_renderer.material.EnableKeyword("_EMISSION");
         else
             m_renderer.material.DisableKeyword("_EMISSION");
     }
 
     public void Interact(IInteractor interactor) {
-        Toggle();
+        Toggle(!IsOn);
     }
 }
